083137930e79829f41cde67883f10350b0f87836
Marcelo R. Bianchi <marcelorbianchi96@gmail.com>
2022-06-16 11:05:02 -0300

    Fix no exports

--- packages/hdwallet-native/src/crypto/isolation/core/ed25519/types.ts
+++ packages/hdwallet-native/src/crypto/isolation/core/ed25519/types.ts
@@ -1,3 +1,4 @@
 //import { Literal, Object as Obj, Static, Union } from "funtypes";
 //export {FieldElement as CurvePoint} from "@iota/crypto.js/src/signatures/edwards25519/fieldElement";
 //export type Signature = Uint8Array;
+export {};
